ea7021b76bd2ec0bc8b4fd838fd7afd9ff0d3b4b
LinDi_ <118484191+gzldsss@users.noreply.github.com>
2023-03-15 13:49:31 +0000

    Add files via upload

---
+++ Work2_maximilian/ofApp.cpp
@@ -0,0 +1,124 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofBackground(128,0,128);
+    
+    //Initialize the drawing variables
+    for (int i = 0; i < ofGetWidth(); ++i) {
+        waveform[i] = 0;
+    }
+    waveIndex = 0;
+    
+    // Maximilian audio stuff
+    int sampleRate = 44100; /* Sampling Rate */
+    int bufferSize= 512; /* Buffer Size. you have to fill this buffer with sound using the for loop in the audioOut method */
+    ofxMaxiSettings::setup(sampleRate, 2, bufferSize);
+    
+    // Setup ofSound
+    ofSoundStreamSettings settings;
+    settings.setOutListener(this);
+    settings.sampleRate = sampleRate;
+    settings.numOutputChannels = 2;
+    settings.numInputChannels = 0;
+    settings.bufferSize = bufferSize;
+    soundStream.setup(settings);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    /////////////// waveform
+    ofTranslate(0, ofGetHeight()/3);
+    ofSetColor(0, 0, 0);
+    ofFill();
+    ofDrawLine(0, 0, 1, waveform[9] * ofGetHeight()/2.); //first line
+    for(int i = 1; i < (ofGetWidth() - 1); ++i) {
+        ofDrawLine(i, waveform[i] * ofGetHeight()/2., i + 1, waveform[i+1] * ofGetHeight()/2.);
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::audioIn(ofSoundBuffer& input){
+    std::size_t nChannels = input.getNumChannels();
+    for (size_t i = 0; i < input.getNumFrames(); i++)
+    {
+        
+    }
+}
+//--------------------------------------------------------------
+void ofApp::audioOut(ofSoundBuffer& output){
+    std::size_t outChannels = output.getNumChannels();
+    for (int i = 0; i < output.getNumFrames(); ++i){
+        
+        output[i * outChannels] = oscillator1.sinewave(20 + oscillator2.saw(20)* 5) + oscillator3.sinewave(50) * oscillator4.saw(4 + oscillator5.saw(0.5) * 50);
+        output[i * outChannels + 1] = output[i * outChannels];
+        
+        //Hold the values so the draw method can draw them
+        waveform[waveIndex] =  output[i * outChannels];
+        if (waveIndex < (ofGetWidth() - 1)) {
+            ++waveIndex;
+        } else {
+            waveIndex = 0;
+        }
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+    
+}
